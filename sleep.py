4d0903c60dc7fb04d671501d7aeb72e6ba9d4abe
PascoPrzyborowski <unity01@gmx.net>
2022-09-14 12:58:32 +0200

    time

---
+++ sleep.py
@@ -0,0 +1,68 @@
+import time
+import os
+from os import system
+
+gt = time.time() #General time
+mt = time.time() # Unix time
+ht = time.ctime(gt) # Human time
+#z = time.gmtime()
+start = time.time()
+
+
+print("--------------------  0%")
+time.sleep(1)
+os.system("clear")
+print("###-----------------  10%")
+time.sleep(1)
+os.system("clear")
+print("######--------------  30%")
+time.sleep(1)
+os.system("clear")
+print("#########----------  50%")
+time.sleep(1)
+os.system("clear")
+print("###############-----  70%")
+time.sleep(1)
+os.system("clear")
+print("##################--  80%")
+time.sleep(1)
+os.system("clear")
+print("###################-  90%")
+time.sleep(1)
+os.system("clear")
+print("###################-  95%")
+time.sleep(2)
+os.system("clear")
+print("###################-  97%")
+time.sleep(2)
+os.system("clear")
+print("###################-  99%")
+time.sleep(2)
+os.system("clear")
+print("######-- Download Completed--######  100%")
+print("Downloads\Files\Confidential\Area51_Truth_Report.txt")
+print()
+# print(mt)
+# print()
+# print(ht)
+print()
+print("Started on:" + ht)
+print()
+
+gte = time.time()
+hte = time.ctime(gte)
+print("Ended on:" + hte)
+print()
+# x = ht
+# y = hte
+# z = time.gmtime()
+stop = time.time()
+print("The time of the run in Seconds:", stop - start)
+# Endof = int(ht - hte)
+# print("Elapsed Time:" + Endof)
+
+
+# time.sleep(2)
+# os.system("clear")
+
+
